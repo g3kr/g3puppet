edc460af2ffa0ff85e5a1f12dd62e87a56861092
Gayatri Swaminathan <g3khasiram@gmail.com>
2014-02-22 11:51:58 -0500

    Deleted files

--- modules/base-mongo/manifests/init.pp
+++
@@ -1,4 +0,0 @@
-class base-mongo(){
-	$public_ip = $::ipaddress
-	include base-mongo::params, base-mongo::install, base-mongo::config
-}
--- modules/mongo/templates/master_replicaset.cmd
+++
@@ -1,1 +0,0 @@
-rs.initiate({"_id" : "<%= replSet %>","members" : [{"_id" : 0,"host" : "<%= hostName %>:<%= port %>","priority" : 50}]})
